4c3571b3d155a89826c411d5b4a71100571e298e
MKA SOMRAT (مْ كِ أ سُمْرَتْ) <mkasomrat@gmail.com>
2025-10-03 16:25:22 +0600

    Add files via upload

---
+++ qr/qr.js
@@ -0,0 +1,10 @@
+﻿(function() {
+      var $value = document.querySelector('form [name="value"]');
+      var qr = new QRious({
+        element: document.getElementById('qr'),
+        value: 'https://mkasomrat.xyz'
+      });
+      $value.addEventListener('keyup', function() {
+      qr.value = $value.value;
+      })
+    })();
